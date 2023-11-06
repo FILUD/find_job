2101b33821d598c711883c47dccff5989be38a0f
TICK-MHK <113336015+FILUD@users.noreply.github.com>
2023-11-06 11:37:38 +0700

    pop-up

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './component/navbar/navbar_welcome';
 import Welcome_Page from './page/Welcome_Page';
 import About_Page from './page/About_Page';
 import Contact_page from './page/Contact_page';
+import Sign_in from './component/popup/Sign_in';
 
 function App() {
   return (
@@ -14,9 +15,13 @@ function App() {
       <div className="App">
         
         <Routes>
+
           <Route index element={<Welcome_Page />} />
           <Route  path="/About" element={<About_Page />} />
           <Route  path="/Contact" element={<Contact_page />} />
+          <Route  path="/Popup_sign_in" element={<Sign_in />} />
+
+
         </Routes>
 
       </div>
---
+++ src/component/popup/Sign_in.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Sign_in.css'
+
+function Sign_in() {
+    return (
+        <div className='big-box-popup'>
+            <div className='container-sign-up'>
+
+                sign_in
+
+            </div>
+        </div>
+    )
+}
+
+
+export default Sign_in
