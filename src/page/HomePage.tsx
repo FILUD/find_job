7beb1e95e025388a96dd60d06a66d82fae8b623b
TICK-MHK <113336015+FILUD@users.noreply.github.com>
2023-12-05 09:29:32 +0700

    create Simple layout Home Page

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import '../css/HomePage.css'
+
+import Navbar from '../component/navbar/Navbar';
+import Footer from '../component/footer/Footer';
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <Navbar />
+
+      <center>
+        <div className="contianer-home-page">
+          <div className="box-haeder-home-page">
+            <p className='text-in-box-haeder-home-page'>Welcome to us website</p>
+          </div>
+          <div className="box-for-text-recommend-job">
+            recommend Job
+          </div>
+
+          <div className="box-item-for-recommend-job">
+            <div className="item-for-recommend-job"></div>
+            <div className="item-for-recommend-job"></div>
+            <div className="item-for-recommend-job"></div>
+            <div className="item-for-recommend-job"></div>
+          </div>
+          <hr className='hr-bottom-recommend-job' />
+          <p>See ALL ▶</p>
+
+
+          <div className="box-for-text-recommend-job">
+            For Find Employee
+          </div>
+          <div className="box-item-for-recommend-job">
+            <div className="item-for-recommend-job"></div>
+            <div className="item-for-recommend-job"></div>
+            <div className="item-for-recommend-job"></div>
+            <div className="item-for-recommend-job"></div>
+          </div>
+          <hr className='hr-bottom-recommend-job' />
+          <p>See ALL ▶</p><br/>
+
+        </div>
+      </center>
+      <Footer/>
+    </div>
   )
 }
 
