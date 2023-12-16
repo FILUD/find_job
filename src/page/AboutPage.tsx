64214f77ab47936915492d6809387e95ae043d69
Phoutthasone Bounthavong <113420049+Georgewassup@users.noreply.github.com>
2023-12-16 15:49:47 +0700

    update: add words

--- src/page/AboutPage.tsx
+++ src/page/AboutPage.tsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import '../css/AboutPage.css'
 import Navbar from '../component/navbar/Navbar_welcome'
+import { useTranslation } from 'react-i18next';
 
 function About_Page() {
+  const [t, i18n] = useTranslation("global");
+
   return (
     <div>
       <Navbar />
       <div>
         <center>
-          <p className='text-title-about'>
-            Welcome to Bridging Talent, your one-stop destination for finding employment opportunities
-            and connecting with talented students eager to contribute their skills and knowledge to your
-            company. Our platform was founded by a group of enthusiastic final-year students from various
-            disciplines, who recognized the need for a more efficient and student-friendly way to connect
-            with potential employers.
-          </p>
+          <p className='text-title-about'>{t("aboutpage.para1")}</p>
           <br />
           <hr className='hr-about' />
 
-          <p className='text-header-developer'>Developer</p></center>
+          <p className='text-header-developer'>{t("aboutpage.developer")}</p></center>
         <div className='box-profile-about'>
 
           <div>
@@ -45,37 +42,24 @@ function About_Page() {
         <div className='box-text-about'>
 
           <div className='box-text-detail-about'>
-            <p className='text-detail-title-about'>For Job Seekers:</p>
-            <p className='text-detail-in-box-about'>Are you on the hunt for your dream job or simply exploring new career
-              opportunities? Our platform offers a user-friendly interface and powerful
-              tools to help you find the job that aligns with your skills, experience,
-              and aspirations. You can create a comprehensive profile, upload your resume,
-              and browse a wide range of job listings from various industries. We also provide resources and guides to enhance your job search, including tips on resume building, interview preparation, and career advice.</p>
+            <p className='text-detail-title-about'>{t("aboutpage.forseeker")}</p>
+            <p className='text-detail-in-box-about'>{t("aboutpage.seeker1")}</p>
           </div>
 
 
           <div className='box-text-detail-about'>
-            <p className='text-detail-title-about'>For Job Seekers:</p>
-            <p className='text-detail-in-box-about'>Finding the right employee for your company is crucial to its success. We understand
-              the challenges of recruitment and offer an easy-to-use platform for posting job
-              openings, reviewing applications, and conducting interviews. Our advanced search
-              and filtering options allow you to quickly identify candidates who meet your specific
-              criteria. We're committed to helping you build a talented and diverse workforce that
-              aligns with your company's goals and values.</p>
-              </div>
-  
+            <p className='text-detail-title-about'>{t("aboutpage.forseeker")}</p>
+            <p className='text-detail-in-box-about'>{t("aboutpage.seeker2")}</p>
+          </div>
+
 
           <div className='box-text-detail-about'>
-          <p className='text-detail-title-about'>For Job Seekers:</p>
-          <p className='text-detail-in-box-about'>At our core, we are dedicated to creating connections that lead to success.
-            We are passionate about helping individuals achieve their career goals and supporting
-            companies in building strong, dynamic teams. Our commitment to facilitating these connections
-            is driven by a desire to contribute to individual growth, company prosperity, and overall economic
-            development.</p>
-            </div>
+            <p className='text-detail-title-about'>{t("aboutpage.forseeker")}</p>
+            <p className='text-detail-in-box-about'>{t("aboutpage.seeker3")}</p>
+          </div>
 
 
-            </div>
+        </div>
       </div>
     </div>
   )
