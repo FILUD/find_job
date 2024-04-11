c8081397a14893719b1ecbb8eb552e7c6421696b
TICK-MHK <113336015+FILUD@users.noreply.github.com>
2024-04-11 13:48:07 +0700

    Update EmployerProfile.tsx

--- src/page/EmployerProfile.tsx
+++ src/page/EmployerProfile.tsx
@@ -1,90 +1,219 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import Navbar from '../component/navbar/Navbar';
+import Footer from '../component/footer/Footer';
 // import '../css/EmpProfile.css'
+
+interface JobDetail {
+  JobID: number,
+  IMG_CV: number,
+  Profile_IMG: string,
+  CompanyName: string,
+  Title: string,
+  Description: string,
+  SalaryStart: number,
+  SalaryMax: number,
+  PostDate: string,
+  OccupationName: string,
+  CategoryName: string,
+  WorkType: string,
+}
+
+interface profileData {
+  EmployerID: number,
+  UserID: number,
+  CompanyName: string,
+  ProfessionalTitle: string,
+  Tel: string,
+  Profile_IMG: string,
+  VillageName: string,
+  DistrictName: string,
+  ProvinceName: string,
+  Email: string,
+}
 
 function EmpProfile() {
 
+  const { employerID } = useParams();
+  const [profileData, setProfileData] = useState<profileData | null>(null);
+  const [jobDetail, setJobDetail] = useState<JobDetail[]>([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedCV, setSelectedCV] = useState<any>(null);
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const response = await axios.post('http://localhost:3001/viewemployer_byid', { employerID });
+        setProfileData(response.data.data[0]);
+      } catch (error) {
+        console.error('Error fetching profile data:', error);
+      }
+    };
+
+    fetchProfileData();
+  }, [employerID]);
+
+  useEffect(() => {
+    const fetchCVDetail = async () => {
+      try {
+        const response = await axios.post('http://localhost:3001/viewjob_byid', { employerID });
+        if (response.data && response.data.length > 0) {
+          setJobDetail(response.data);
+          console.log(setJobDetail);
+        } else {
+          console.error('No CV details found for this jobseeker.');
+        }
+      } catch (error) {
+        console.error('Error fetching CV detail:', error);
+      }
+    };
+
+    fetchCVDetail();
+  }, [employerID]);
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  const handleCardClick = (cv: any) => {
+    setSelectedCV(cv);
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+    setSelectedCV(null);
+  };
+
+  const openFullScreen = (imageUrl: string) => {
+    const fullScreenImage = document.getElementById('fullScreenImage');
+    if (fullScreenImage) {
+      fullScreenImage.addEventListener('click', () => {
+        if (!document.fullscreenElement) {
+          fullScreenImage.requestFullscreen().catch((err) => {
+            console.error('Error attempting to enable full screen:', err);
+          });
+        } else {
+          document.exitFullscreen().catch((err) => {
+            console.error('Error attempting to exit full screen:', err);
+          });
+        }
+      });
+    }
+  };
+
+  if (!profileData || jobDetail.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
-    <center className='createEmpProfile-head'>
-      <div className='createEmpProfile-container'>
-        <div className='createEmpProfile-container-top'>
-          <div className='createEmpProfile-container-top-left'>
-            <img className='user-profile-profile-page' src="Icon/profile-101.png" alt="Profile" />
-          </div>
-          <div className='createEmpProfile-container-top-center-left'>
-            <p className='EmpProfile-full-name'>George Bounthavong</p>
-            <p className='EmpProfile-position-job'>Developer and computer engineering</p>
-            <p className='EmpProfile-address'>
-              <img src="Icon/icon-location.png"
-                className='EmpProfile-icon-location'
-                alt="Location" />Ban Donnoun, Vientiane, Laos</p>
-          </div>
-          <div className='createEmpProfile-container-top-center-right'>
-            <button onClick={() => navigate('/CreateEmpProfile')} className='createEmpProfile-button-save'>Edit My Profile</button>
-          </div>
-          <div className='createEmpProfile-container-top-right'>
-            <img className='createEmpProfile-btn-close' onClick={() => navigate('/Home')} src="Icon/close.png" alt="Close" />
-          </div>
-        </div>
-        <div className='createEmpProfile-container-bottom'>
-
-          <div className='createEmpProfile-container-bottom-left'>
-            <div className='EmpProfile-grid-small-contact'>
-              <div className='EmpProfile-box-icon-font-contact'>
-                <img className='EmpProfile-icon-font-contact' src="Icon/icon-call.png" alt="Phone" />
-              </div>
-              <div className='EmpProfile-box-phone-number'>
-                <p className='EmpProfile-phone-number'>020 99999999</p>
-                <p className='EmpProfile-phone-number'>020 99999999</p>
-                <p className='EmpProfile-phone-number'>020 99999999</p>
-              </div>
+    <div>
+      <Navbar />
+      <div className='container mt-5 mx-auto'>
+        <div className='grid grid-cols-3 gap-2'>
+          {/* Profile Information */}
+          <div className='p-20 grid bg-zinc-900 rounded-3xl justify-items-center justify-center'>
+            {/* Profile Image */}
+            {profileData.Profile_IMG ? (
+              <img className='bg-cover w-56 rounded-full border-2 border-slate-50' src={profileData.Profile_IMG} alt="Profile" />
+            ) : (
+              <img className='bg-cover w-56 rounded-full border-2 border-slate-50' src="/Icon/profile.png" alt="Default Profile" />
+            )}
+            <h1 className='pt-5 pb-5 text-center'>{profileData.CompanyName}</h1>
+            {/* Profile Details */}
+            <div className='px-10'>
+              <p className='text-left'><b>Email:</b> {profileData.Email}</p>
+              <p className='text-left'><b>Tel:</b> {profileData.Tel}</p>
+              <p className='text-left'><b>Address:</b> {profileData.VillageName}, {profileData.DistrictName}, {profileData.ProvinceName}</p>
             </div>
-            <div className='EmpProfile-grid-small-contact'>
-              <div className='EmpProfile-box-icon-font-contact'>
-                <img className='EmpProfile-icon-font-contact' src="Icon/icon-mail.png" alt="Email" />
-              </div>
-              <div className='EmpProfile-box-gmail'>
-                <p className='EmpProfile-gmail'>george@gg.com</p>
-                <p className='EmpProfile-gmail'>george@ggg.com</p>
-                <p className='EmpProfile-gmail'>george@gggg.com</p>
-              </div>
+            {/* EmployerID: number,
+            UserID: number,
+            CompanyName: string,
+            ProfessionalTitle: string,
+            Tel: string,
+            Profile_IMG: string,
+            VillageName: string,
+            DistrictName: string,
+            ProvinceName: string,
+            Email: string, */}
+          </div>
+
+          {/* Professional Title and Description */}
+          <div className='grid bg-zinc-900 rounded-3xl justify-items-center justify-center'>
+            <div className='bg-slate-50 card w-96 my-5'>
+              <p className='w-full bg-slate-700 rounded-t-2xl text-center'><b>Professional Title:</b> </p>
+              <p className='px-10 text-slate-950'>{profileData.ProfessionalTitle}</p>
+            </div>
+            <div className='bg-slate-50 card w-96 my-5'>
+              <p className='w-full bg-slate-700 rounded-t-2xl text-center'><b>Description:</b> </p>
+              <p className='px-10 text-slate-950'>{profileData.Description}</p>
             </div>
           </div>
-          <div className='createEmpProfile-container-bottom-center'>
-            <div className='createEmpProfile-head-box'>
-              <p className='createEmpProfile-container-bottom-center-text-header'>Description</p></div>
-            <div className='createEmpProfile-box'>
-              <div className='EmpProfile-box-description'>
-                <p className='EmpProfile-description'>ttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttestesttesttest</p>
-              </div>
-            </div>
-          </div>
-          <div className='createEmpProfile-container-bottom-right'>
-            <div className='createEmpProfile-container-bottom-center'>
-              <div className='createEmpProfile-head-box'>
-                <p className='createEmpProfile-container-bottom-center-text-header'>My CV</p></div>
-              <div className='createEmpProfile-box createEmpProfile-box-cv'>
-                <div className='createEmpProfile-input-container createEmpProfile-cv-content'>
 
-                  <center>
-                    <div className='emp-profile-pdf'>
-                     
-                    </div>
-                  </center>
+          <div className='grid bg-zinc-900 rounded-3xl justify-items-center justify-center' style={{ overflowY: 'auto' }}>
 
-                  {/* <img className='createEmpProfile-cv-post-pdf' src="Icon/btn-add-pdf.png" alt="ADD PDF" />
-                  <div className='vertical-line'></div>
-                  <button className='createEmpProfile-btn-create-cv'>Create My CV</button> */}
+            {cvDetail && (
+              <div>
+                <div className='bg-slate-700 rounded-t-2xl py-1'>
+                  <h2 className='text-center'>CV</h2>
                 </div>
+                {cvDetail.map((cv: any) => (
+                  <div key={cv.CvID} onClick={() => handleCardClick(cv)} className='grid grid-cols-3 bg-slate-700 rounded-2xl my-2 mx-1'>
+                    <figure className='col-span-1 p-5 rounded-xl '>
+                      {cv.IMG_CV && <img className='bg-cover rounded-xl' src={cv.IMG_CV} alt="IMG_CV" />}
+                    </figure>
+
+                    <div className='col-span-2 py-5'>
+                      <p>Title: {cv.Title}</p>
+                      <p className='text-left'>Posted: {cv.UploadDate ? formatDate(cv.UploadDate) : 'N/A'}</p>
+                      <p>Occupation : {cv.OccupationName}</p>
+                      <p>Category : {cv.CategoryName}</p>
+                    </div>
+                  </div>
+                ))}
               </div>
-            </div>
+            )}
+
+            {selectedCV && (
+              <dialog id="my_modal_3" className="modal" open>
+                <div className="modal-box">
+                  <button className="btn btn-sm btn-square btn-ghost absolute right-2 top-2" onClick={closePopup}>✕</button>
+                  <div className='bg-stone-800 rounded-2xl py-10 justify-center justify-items-center grid'>
+                    <figure className='w-40 '>
+                      <div className="card w-75 bg-base-100 shadow-xl" key={selectedCV.CvID} onClick={() => handleCardClick(selectedCV)}>
+                        <img id="fullScreenImage" className='bg-cover rounded-2xl' src={selectedCV.IMG_CV} alt="IMG_CV" onClick={() => openFullScreen(selectedCV.IMG_CV)} />
+                      </div>
+                    </figure>
+                  </div>
+                  <div className="card-body bg-stone-800  rounded-2xl">
+                    <div className='w-full flex justify-self-end justify-items-end justify-end -mt-7 ml-7'>
+                    </div>
+                    <p className='text-left'><b>{selectedCV.Title}</b></p>
+                    <p className='text-left'><u>Occupation :</u>{selectedCV.OccupationName}</p>
+                    <p className='text-left'><u>Work category :</u> {selectedCV.CategoryName}</p>
+                    <p className='text-left'><u>Posted:</u> {selectedCV.UploadDate ? formatDate(selectedCV.UploadDate) : 'N/A'}</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Apply</button>
+                    </div>
+                  </div>
+                </div>
+              </dialog>
+            )}
+
+
           </div>
         </div>
       </div>
-    </center>
+      <Footer />
+    </div>
   )
 }
 
