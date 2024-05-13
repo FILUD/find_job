f10a4b6594df7357b04624e3bffe602e04c1d124
Phoutthasone Bounthavong <113420049+Georgewassup@users.noreply.github.com>
2024-05-14 01:05:32 +0700

    Update HomePage.tsx

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -507,7 +507,7 @@ function HomePage() {
 
           {isOpenJobReq && (
             <div className='absolute'>
-              <JobRequest isOpen={isOpenJobReq} isClose={closeToggleJobRequest} senderId={dataList.senderId} receiverImg={dataList.receiverImg} receiverId={dataList.receiverId} jobId={dataList.jobId} receiverName={dataList.receiverName} jobTitle={dataList.jobTitle} category={dataList.category} type={dataList.type}/>
+              <JobRequest isOpen={isOpenJobReq} isClose={closeToggleJobRequest} senderId={dataList.senderId} receiverImg={dataList.receiverImg} receiverId={dataList.receiverId} jobId={dataList.jobId} receiverName={dataList.receiverName} jobTitle={dataList.jobTitle} category={dataList.category} type={dataList.type} />
             </div>
           )}
 
@@ -563,7 +563,6 @@ function HomePage() {
                           </svg>
                         </button>
 
-<<<<<<< Updated upstream
                         <button className="btn btn-primary">
                           <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
@@ -574,12 +573,11 @@ function HomePage() {
                         <button className="btn btn-primary" onClick={() => openProfileJOB(selectedJOB.EmployerID)}>View Profile</button>
                       </>
                     )}
-=======
-                    <button className="btn btn-primary" onClick={() => handleIsOpenJobRequest()}>Apply</button>
+                    {/* todo */}
+                    {/* <button className="btn btn-primary" onClick={() => handleIsOpenJobRequest()}>Apply</button>
                     {/* <button className="w-full btn btn-primary bg-purple-600"  >Apply</button> */}
 
-                    <button className="btn btn-primary" onClick={() => openProfileJOB(selectedJOB.EmployerID)}>View Profile</button>
->>>>>>> Stashed changes
+                    {/* <button className="btn btn-primary" onClick={() => openProfileJOB(selectedJOB.EmployerID)}>View Profile</button> */}
 
                   </div>
                 </div>
